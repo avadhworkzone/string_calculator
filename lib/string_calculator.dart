86fba0bd821e471308d8c2113b38a6437df96077
developer <>
2026-01-05 21:17:42 +0530

    GREEN: make custom delimiters test pass

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,8 +4,17 @@ class StringCalculator {
       return 0;
     }
     
-    String normalized = numbers.replaceAll('\n', ',');
-    List<String> parts = normalized.split(',');
+    String delimiter = ',';
+    String numbersToProcess = numbers;
+    
+    if (numbers.startsWith('//')) {
+      int newlineIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, newlineIndex);
+      numbersToProcess = numbers.substring(newlineIndex + 1);
+    }
+    
+    String normalized = numbersToProcess.replaceAll('\n', delimiter);
+    List<String> parts = normalized.split(delimiter);
     int sum = 0;
     for (String part in parts) {
       sum += int.parse(part);
