9860e4eeae3b0b5a92c6742a432899a0732a5a04
developer <>
2026-01-05 21:14:39 +0530

    RED: add failing test for custom delimiters

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -21,4 +21,9 @@ void main() {
     final calculator = StringCalculator();
     expect(calculator.add('1\n2,3'), 6);
   });
+
+  test('custom delimiters work', () {
+    final calculator = StringCalculator();
+    expect(calculator.add('//;\n1;2'), 3);
+  });
 }
